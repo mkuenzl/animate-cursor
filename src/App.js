d6947885858d3b85efda85b4299aa4a2aba47b1f
Moritz Künzl <mo.kuenzl@gmail.com>
2022-08-21 16:35:11 +0200

    changed blocks visualisation to eyes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
-import { useRef } from 'react';
 import Block from './Block';
 
 function App() {
   
-  // const ref = useRef(null);
-  // const mouseListener = (e) => {
-  //   saveCursorPosition(e.clientX, e.clientY);
-  // };
-
-  // ref.current.addEventListener();
-  
   return (
-    <html>
+    <>
       <Block /> 
-    </html>
+    </>
   );
 }
 
