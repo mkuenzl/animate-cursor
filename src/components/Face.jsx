f126166abe32bc28f1303945b4d32af1a0589696
Moritz Künzl <mo.kuenzl@gmail.com>
2022-08-27 13:53:18 +0200

    changed container proportions from vw to %, added reference position of the outer container

--- src/components/Face.jsx
+++ src/components/Face.jsx
@@ -9,7 +9,7 @@ function Face() {
 
 	return (
 		<div className="container">
-			<div className="face">
+			<div className="face" ref={setPosition}>
 				{JSON.stringify(mousePosition)}
 				<div className="eyebrows">
 					<Eyebrow />
@@ -28,6 +28,37 @@ function Face() {
 }
 
 export default Face;
+
+function setPosition(el) {
+	// el can be null - see https://reactjs.org/docs/refs-and-the-dom.html#caveats-with-callback-refs
+	if (!el) return;
+
+	const responsiveX = (
+		el.getBoundingClientRect().x / window.innerWidth
+	).toFixed(2);
+	const responsiveY = (
+		el.getBoundingClientRect().y / window.innerHeight
+	).toFixed(2);
+
+	console.log(window.innerHeight);
+
+	const containerCenterX = (
+		el.getBoundingClientRect().width / 2 / window.innerWidth +
+		Number(responsiveX)
+	).toFixed(2);
+	console.log(containerCenterX);
+
+	const containerCenterY = (
+		el.getBoundingClientRect().width / 2 / window.innerHeight +
+		Number(responsiveY)
+	).toFixed(2);
+	console.log(containerCenterY);
+
+	const root = document.getElementById("root");
+
+	root.style.setProperty("--positionX", containerCenterX);
+	root.style.setProperty("--positionY", containerCenterY);
+}
 
 function useMousePosition() {
 	const [mousePosition, setMousePosition] = useState({ x: null, y: null });
